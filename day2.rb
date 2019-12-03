a63ebb8d65da01e2b7eb35b32e4740152864e7ee
Tobias Eilert <tobias.eilert@grandcentrix.net>
2019-12-03 15:12:52 +0100

    Day 2 part 1

---
+++ day2.rb
@@ -0,0 +1,84 @@
+require_relative "helpers"
+
+class OpCodeMachine
+  attr_accessor :opcodes
+  attr_reader :current_position, :result
+
+  def initialize(opcodes=[])
+    if opcodes.is_a?(String)
+      @opcodes = parse(opcodes)
+    else
+      @opcodes = opcodes
+    end
+
+    @current_position = 0
+    @result = 0
+  end
+
+  def find_opcode
+    opcode = @opcodes[@current_position]
+
+    raise "Unkown Opcode #{opcode} at pos: #{@current_position}" unless [1, 2, 99].include?(opcode)
+    opcode
+  end
+
+  def find_operands
+    [@opcodes[read_rel(1)], @opcodes[read_rel(2)]]
+  end
+
+  def find_target_index
+    read_rel(3)
+  end
+
+  def handle_instruction!
+    instruction = find_opcode()
+
+    return :halt if instruction == 99
+
+    operands = find_operands()
+    operation = instruction == 1 ? :+ : :*
+
+    @result = operands[0].send(operation, operands[1])
+    @opcodes[find_target_index()] = @result
+
+    @opcodes
+  end
+
+  def move!
+    @current_position += 4
+  end
+
+  def run!
+    loop do
+      break if handle_instruction! == :halt
+      move!
+    end
+
+    @result
+  end
+
+  private
+
+  def read_rel(pos)
+    @opcodes[@current_position + pos]
+  end
+
+  def parse(string_codes)
+    string_codes.split(",").map(&:to_i)
+  end
+end
+
+
+def solution
+  opcodes = File.read(filepath("day2.txt"))
+  machine = OpCodeMachine.new(opcodes)
+
+  machine.opcodes[1] = 12
+  machine.opcodes[2] = 2
+
+  machine.run!
+end
+
+if __FILE__ == $0
+  puts solution()
+end
---
+++ spec/day2_spec.rb
@@ -0,0 +1,108 @@
+require "rspec"
+require_relative "../day2"
+
+describe "day2" do
+  context "initialize machine" do
+    it "converts a string of integers to a list of integers" do
+      machine = OpCodeMachine.new("0,1,2,3,0")
+      expect(machine.opcodes).to eq([0, 1, 2, 3, 0])
+    end
+
+    it "trims newlines and whitespace" do
+      machine = OpCodeMachine.new("0     ,  1,2,3    ,0\n")
+      expect(machine.opcodes).to eq([0, 1, 2, 3, 0])
+    end
+
+    it "starts with a position of 0" do
+      machine = OpCodeMachine.new()
+      expect(machine.current_position).to eq(0)
+      expect(machine.result).to eq(0)
+    end
+  end
+
+  context "opcodes" do
+    it "finds the opcode" do
+      machine = OpCodeMachine.new([1,0,0,3,99])
+      expect(machine.find_opcode()).to eq(1)
+    end
+
+    it "allows 1, 2 and 99 as opcodes" do
+      [1, 2, 99].each do |opcode|
+        machine = OpCodeMachine.new([opcode])
+        expect(machine.find_opcode()).to eq(opcode)
+      end
+    end
+
+    it "will throw an error if an unknown opcode is found" do
+      (3..98).each do |opcode|
+        machine = OpCodeMachine.new([opcode])
+
+        expect { machine.find_opcode() }.to raise_error("Unkown Opcode #{opcode} at pos: 0")
+      end
+    end
+
+    it "finds the operands for an instruction" do
+      machine = OpCodeMachine.new([1,0,0,3,99])
+      expect(machine.find_operands()).to eq([1, 1])
+
+      machine = OpCodeMachine.new([1,2,0,3,99])
+      expect(machine.find_operands()).to eq([0, 1])
+    end
+
+    it "finds the target for an instruction" do
+      machine = OpCodeMachine.new([1,0,0,3,99])
+      expect(machine.find_target_index()).to eq(3)
+
+      machine = OpCodeMachine.new([1,0,0,8,99])
+      expect(machine.find_target_index()).to eq(8)
+    end
+
+    it "can move" do
+      machine = OpCodeMachine.new([1,0,0,3,99])
+      expect(machine.current_position).to eq(0)
+
+      machine.move!
+      expect(machine.current_position).to eq(4)
+      expect(machine.find_opcode()).to eq(99)
+    end
+
+    it "adds" do
+      machine = OpCodeMachine.new([1,0,0,3,99])
+      expect(machine.handle_instruction!()).to eq([1, 0, 0, 2, 99])
+      expect(machine.result).to eq(2)
+    end
+
+    it "multiplies" do
+      machine = OpCodeMachine.new([2,0,3,3,99])
+      expect(machine.handle_instruction!()).to eq([2, 0, 3, 6, 99])
+      expect(machine.result).to eq(6)
+    end
+
+    it "halts" do
+      original_opcodes = [99, 1, 0, 0, 1, 0, 2, 0, 5, 0]
+      machine = OpCodeMachine.new(original_opcodes)
+
+      expect(machine.handle_instruction!()).to eq(:halt)
+
+      # opcodes are not modified when the instruction says halt
+      expect(machine.opcodes).to eq(original_opcodes)
+    end
+
+    it "runs until it finds a halt instruction and yields the result" do
+      programs = [
+        { input: [1,0,0,0,99], expected_result: 2, expected_state: [2,0,0,0,99] },
+        { input: [2,3,0,3,99], expected_result: 6, expected_state: [2,3,0,6,99] },
+        { input: [2,4,4,5,99,0], expected_result: 9801, expected_state: [2,4,4,5,99,9801] },
+        { input: [1,1,1,4,99,5,6,0,99], expected_result: 30, expected_state: [30,1,1,4,2,5,6,0,99] },
+      ]
+
+      programs.each do |program|
+        machine = OpCodeMachine.new(program[:input])
+        expect(machine.run!).to eq(program[:expected_result])
+        expect(machine.opcodes).to eq(program[:expected_state])
+      end
+    end
+  end
+end
+
+
